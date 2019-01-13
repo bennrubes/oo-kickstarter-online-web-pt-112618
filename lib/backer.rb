a468f600ef4cef7912d12644958aa82b43e2a4bc
Sam Bennrubi <sbennrubi@gmail.com>
2019-01-13 21:19:07 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,5 +1,6 @@
 class Backer
   attr_accessor :name
+  attr_reader :backed_projects
   
   def initialize(name)
     @name = name
@@ -8,10 +9,6 @@ class Backer
 
   def back_project(project)
     @backed_projects << project
-  end
-
-  def backed_projects
-    @backed_projects
   end
   
 end
